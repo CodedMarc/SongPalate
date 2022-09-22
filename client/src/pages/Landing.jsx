64c642b7c904e92d8dafce101d1651d2b3fc051c
CodedMarc <doramm4408@gmail.com>
2022-09-21 21:04:06 -0500

    made login btn bigger

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -5,7 +5,7 @@ function Landing() {
   return (
     <main className="Landing">
       <h1 className="title">Song Palate</h1>
-      <p className='subtext center'>A Better Way to Find New Music</p>
+      <p className='subtext center'>A (soon to be) Better Listening Experience</p>
       <a href="/auth/spotify" className="spotify-btn"><span className="fa-brands fa-spotify"></span> Login With Spotify</a>
     </main>
   );
