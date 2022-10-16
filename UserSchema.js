a250166bec53d2b8b9d6bb71453fcbab71f71e67
CodedMarc <doramm4408@gmail.com>
2022-10-16 16:51:17 -0500

    added name to schema

--- UserSchema.js
+++ UserSchema.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const findOrCreate = require('mongoose-findorcreate')
 
 const userSchema = new mongoose.Schema({
+  name: String,
   username: String,
   email: String,
   token: String,
